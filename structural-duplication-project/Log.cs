f28d3c54a3c439ccb95915120434088c7a1b8af8
Aleksandr Lebedev <alexandr.2nd@gmail.com>
2025-10-26 11:56:16 +0100

    added logger class

---
+++ structural-duplication-project/Log.cs
@@ -0,0 +1,71 @@
+using System;
+using System.IO;
+
+namespace StructuralDuplication;
+
+public enum LogLevel { Trace = 0, Debug = 1, Info = 2, Warn = 3, Error = 4, Fatal = 5 }
+
+public static class log
+{
+    static readonly object _sync = new();
+    static LogLevel _min = LogLevel.Info;
+    static TextWriter? _file;
+    static bool _useColors = true;
+
+    public static void setup(LogLevel minimum = LogLevel.Info, string? filePath = null, bool useColors = true)
+    {
+        _min = minimum;
+        _useColors = useColors;
+        if (!string.IsNullOrWhiteSpace(filePath))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(Path.GetFullPath(filePath))!);
+            _file = TextWriter.Synchronized(new StreamWriter(filePath, append: true) { AutoFlush = true });
+        }
+        info("logger initialized (min={0}{1})",
+            minimum,
+            filePath is null ? "" : $", file='{Path.GetFullPath(filePath)}'");
+        AppDomain.CurrentDomain.ProcessExit += (_, __) => _file?.Dispose();
+        Console.CancelKeyPress += (_, __) => _file?.Dispose();
+    }
+
+    public static void trace(string msg, params object[] a) => Write(LogLevel.Trace, msg, a);
+    public static void debug(string msg, params object[] a) => Write(LogLevel.Debug, msg, a);
+    public static void info (string msg, params object[] a) => Write(LogLevel.Info , msg, a);
+    public static void warn (string msg, params object[] a) => Write(LogLevel.Warn , msg, a);
+    public static void error(string msg, params object[] a) => Write(LogLevel.Error, msg, a);
+    public static void error(Exception ex, string msg = "error", params object[] a) => Write(LogLevel.Error, msg + " :: " + ex, a);
+    public static void fatal(string msg, params object[] a) => Write(LogLevel.Fatal, msg, a);
+    public static void fatal(Exception ex, string msg = "fatal", params object[] a) => Write(LogLevel.Fatal, msg + " :: " + ex, a);
+
+    static void Write(LogLevel level, string msg, params object[] a)
+    {
+        if (level < _min) return;
+        var ts = DateTime.Now.ToString("HH:mm:ss");
+        var lvl = level.ToString().ToUpper();
+        var line = a is { Length: > 0 } ? string.Format(msg, a) : msg;
+
+        lock (_sync)
+        {
+            if (_useColors) PushColor(level);
+            Console.WriteLine($"[{ts} {lvl}] {line}");
+            if (_useColors) Console.ResetColor();
+
+            _file?.WriteLine($"[{ts} {lvl}] {line}");
+        }
+    }
+
+    static void PushColor(LogLevel level)
+    {
+        ConsoleColor c = level switch
+        {
+            LogLevel.Trace => ConsoleColor.DarkGray,
+            LogLevel.Debug => ConsoleColor.Gray,
+            LogLevel.Info  => ConsoleColor.White,
+            LogLevel.Warn  => ConsoleColor.Yellow,
+            LogLevel.Error => ConsoleColor.Red,
+            LogLevel.Fatal => ConsoleColor.Magenta,
+            _ => ConsoleColor.White
+        };
+        Console.ForegroundColor = c;
+    }
+}
--- structural-duplication-project/Program.cs
+++ structural-duplication-project/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Serilog;
 
-Console.WriteLine("Hello, World!");
+namespace StructuralDuplication;
+class Program
+{
+    static void Main(string[] args)
+    {   
+        log.setup(minimum: LogLevel.Info);
+        
+        if (args.Length == 0)
+        {
+            log.error("Usage: dotnet run <path_to_file_or_directory>. Exiting..");
+            return;
+        }
+        string path = args[0];
+
+        if (File.Exists(path))
+        {
+            log.info("File found: {0}", Path.GetFullPath(path));
+        }
+        else if (Directory.Exists(path))
+        {
+            log.info($"Directory found {0}: {Path.GetFullPath(path)}");
+        }
+        else
+        {
+            log.error("Path not found: {0}", path);
+        }
+    }
+}
